dcf9155d103843c8cc936c558be4129f5908d8bc
Pranay Kandikonda <pranayk74174174@gmail.com>
2026-02-08 23:55:08 +0530

    minor changes in ArtistCard.jsx

--- src/components/artistCard/ArtistCard.jsx
+++ src/components/artistCard/ArtistCard.jsx
@@ -9,19 +9,26 @@ function ArtistCard ({setCurrentPage,setCurrentEle}){
            <div className="artistCardMainContainer">
            {
                artistCardData.map((ele,id)=>{
+                
                    
                    return(
                        <>
                        <div key={id} onClick={()=>{
                         setCurrentPage("artistSpecificPage")
                         setCurrentEle(ele)
-                       }} className="AcardContainer">
+                       }} className="AcardContainer ">
+
+
                            <img src={ele.imgSrc} alt="img"/>
-                           <div className="AcardTextContainer">
+                           <div className=" AcardTextContainer">
                                <h2>{ele.heading}</h2>
+
+
                                <p>{ele.subHeading}</p>
                            </div>
                            <span id="playButton"><svg data-encore-id="icon" role="img" aria-hidden="true" class="e-9812-icon e-9812-baseline" viewBox="0 0 24 24"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path></svg></span>
+
+
 
                        </div>
 
